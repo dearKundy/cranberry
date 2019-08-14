4eade903c0fee89e0151c599f54070ce3962458a
kundyli <kundyli@163.com>
2019-08-14 18:11:59 +0800

    First commit

---
+++ src/main/java/com/kundy/justbattle/JustBattleApplication.java
@@ -0,0 +1,13 @@
+package com.kundy.justbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustBattleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustBattleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kundy/justbattle/JustBattleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kundy.justbattle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JustBattleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
