a42d3347ce8397ec2cae95d1158b3bea6c4b8bad
kundyli <kundyli@163.com>
2019-08-19 15:49:30 +0800

    feat:基于redis实现限流

--- src/main/java/com/kundy/justbattle/model/po/JbUserPo.java
+++ src/main/java/com/kundy/justbattle/model/po/JbUserPo.java
@@ -3,13 +3,15 @@ package com.kundy.justbattle.model.po;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
+
 /**
  * @author kundy
  * @date 2019/8/16 5:12 PM
  */
 @Data
 @Accessors(chain = true)
-public class JbUserPo {
+public class JbUserPo implements Serializable {
 
     private int id;
     private String name;
--- src/test/java/com/kundy/justbattle/JustBattleApplicationTests.java
+++ src/test/java/com/kundy/justbattle/JustBattleApplicationTests.java
@@ -3,6 +3,7 @@ package com.kundy.justbattle;
 import com.kundy.justbattle.dblock.OptimismLock;
 import com.kundy.justbattle.dblock.PessimisticLock;
 import com.kundy.justbattle.model.po.JbUserPo;
+import com.kundy.justbattle.ratelimit.RedisRateLimiter;
 import com.kundy.justbattle.transaction.AnnotationTx;
 import com.kundy.justbattle.transaction.ProgrammingTx;
 import com.kundy.justbattle.transaction.TemplateTx;
@@ -10,6 +11,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.CountDownLatch;
@@ -33,6 +36,12 @@ public class JustBattleApplicationTests {
     @Autowired
     private OptimismLock optimismLock;
 
+    @Autowired
+    private RedisRateLimiter limiter;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
     public void testProgramingTx() {
         JbUserPo jbUserPo = new JbUserPo().setName("heyJude").setPassword("0000");
@@ -41,21 +50,21 @@ public class JustBattleApplicationTests {
     }
 
     @Test
-    public void testTemplateTx(){
+    public void testTemplateTx() {
         JbUserPo jbUserPo = new JbUserPo().setName("yahu").setPassword("0000");
         boolean flag = this.templateTx.go(jbUserPo);
         System.out.println(flag);
     }
 
     @Test
-    public void testAnnotationTx(){
+    public void testAnnotationTx() {
         JbUserPo jbUserPo = new JbUserPo().setName("yiming").setPassword("0000");
         boolean flag = this.annotationTx.go(jbUserPo);
         System.out.println(flag);
     }
 
     @Test
-    public void testDbLock(){
+    public void testDbLock() {
         CountDownLatch latch = new CountDownLatch(5);
 
         for (int i = 0; i < 10; i++) {
@@ -69,7 +78,33 @@ public class JustBattleApplicationTests {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    public void testLimiter() {
+
+        for (int i = 0; i < 1; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (limiter.tryAcquire("rate.limit:key1", 50)) {
+                        System.out.println("pass...");
+                    } else {
+                        System.out.println("no pass...");
+                    }
+                }
+            }).start();
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
